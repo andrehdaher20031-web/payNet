cb974aef2bf7e6d2875ef2a2475323433fc7c612
elidaher <eeli56315@gmail.com>
2026-01-06 14:35:59 +0300

    email

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -83,7 +83,7 @@ router.post('/adminPayInternet', async (req, res) => {
     }
 
     // جلب حساب الأدمن
-    const user = await User.findOne({ username: 'daheradmin' });
+    const user = await User.findOne({ email: 'daheradmin' });
     if (!user) {
       return res.status(404).json({ message: 'حساب الأدمن غير موجود' });
     }
